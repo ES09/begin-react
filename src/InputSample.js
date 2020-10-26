8448e8147887871c26c2ed122450213e2e1a1f21
ES09 <e.song925@gmail.com>
2020-10-26 10:37:46 +0900

    input테스트

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,22 +1,36 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 function InputSample() {
-    const [text, setText] = useState('');
+    const [inputs, setInputs] = useState({
+        name: '',
+        nickname: ''
+    });
+
+    const { name, nickname } = inputs;
 
     const onChange = (e) => {
-        setText(e.target.value);
+        const { value, name } = e.target;
+        setInputs({
+            ...inputs, //기존의 input 객체 복사 : spread문법
+            [name] : value
+        });
     };
-
+  
     const onReset = () => {
-        setText('');
+        setInputs({
+            name : '',
+            nickname : '',
+        })
     };
 
     return (
         <div>
-            <input onChange={onChange} value={text}  />
+            <input placeholder="이름" onChange={onChange} value={name}/>
+            <input placeholder="닉네임" onChange={onChange} value={nickname}/>
             <button onClick={onReset}>초기화</button>
             <div>
-                <b>값: {text}</b>
+                <b>값: </b>
+                {name} ({nickname})
             </div>
         </div>
     );
